0beca745e85e5e5cfbf3307db6b497769bd96e26
masayakumgawa <127034492+masayakumgawa@users.noreply.github.com>
2023-06-06 18:33:53 +0900

    Delete ecsite directory

--- ecsite/src/main/java/jp/co/internous/ecsite/model/dao/GoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.co.internous.ecsite.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jp.co.internous.ecsite.model.entity.Goods;
-
-public interface GoodsRepository extends JpaRepository<Goods, Long>{
-
-}
